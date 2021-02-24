bba3c0a2fba66639bf6bce71ddf2096ca0275b1f
linchenee <44537566+linchenee@users.noreply.github.com>
2021-02-24 22:14:30 +0800

    Update demo_video.m

--- demo_video.m
+++ demo_video.m
@@ -6,7 +6,7 @@ addpath(genpath('Utilities'));
 
 load('claire_qcif.mat');
 
-%% Scenario generation
+% Scenario generation
 [m1,m2,m3] = size(I);
 sampling_ratio = 0.1;
 omega = find(rand(m1 * m2 * m3,1) < sampling_ratio); % locations of the available entries.
@@ -15,7 +15,7 @@ Y = zeros(m1,m2,m3);                                 % incomplete image
 W(omega) = 1;
 Y(omega) = I(omega);
 
-%% Appropriate parameter adjustment can yield a better PSNR result.
+% Appropriate parameter adjustment can yield a better PSNR result.
 opts.lambda = 3.5e6;  
 opts.rank = 90;
 opts.epsilon = 1e3;
@@ -24,7 +24,7 @@ opts.max_iter = 400;
 opts.tol = 1e-3; 
 opts.trunc = 0.3;
 
-%% Video inpainting
+% Video inpainting
 tic
 X = LRTF(Y,W,opts);
 toc
