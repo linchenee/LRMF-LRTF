da8b28091a66ee8ccabb53d4f6717f608ee99e6e
linchenee <44537566+linchenee@users.noreply.github.com>
2021-02-24 22:15:11 +0800

    Update LRMF.m

--- Utilities/LRMF.m
+++ Utilities/LRMF.m
@@ -27,14 +27,14 @@ if isfield(opts,'tol');       tol = opts.tol;             else tol = 1e-3;      
 
 [m1,m2] = size(Y); 
 
-%% Initialization
+% Initialization
 X1 = randn(m1,rank);  X1_old = X1;
 X2 = randn(rank,m2);  X2_old = X2; 
 mu = ones(1,2);       mu_old = mu; 
 mu_min = 1e-3;
 t_old = 1;
 
-%% Iteration
+% Iteration
 for i = 1 : max_iter  
   if mod(i,3) == 1                                  
     t = (1 + sqrt(1 + 4 * t_old^2)) / 2;
@@ -42,7 +42,7 @@ for i = 1 : max_iter
     t_old = t; 
   end
    
-  %% Update X1
+  % Update X1
   w = min(temp,sqrt(mu_old(1) / mu(1)));
   X1_extrapol = X1 + w * (X1 - X1_old);  
   X1_old = X1;
@@ -52,7 +52,7 @@ for i = 1 : max_iter
   X1 = proximal_log(A1,lambda / mu(1),epsilon);
   tols(1) =  norm(X1 - X1_old,'fro') / norm(X1_old,'fro');           
    
-  %% Update X2
+  % Update X2
   w = min(temp,sqrt(mu_old(2) / mu(2)));
   X2_extrapol = X2 + w * (X2 - X2_old);    
   X2_old = X2;
@@ -62,7 +62,7 @@ for i = 1 : max_iter
   X2 = proximal_log(A2,lambda / mu(2),epsilon);
   tols(2) = norm(X2 - X2_old,'fro') / norm(X2_old,'fro');   
     
-  %% Stop if the termination condition is met
+  % Stop if the termination condition is met
   if mean(tols) < tol
     fprintf('Iteration number of LRMF is %d\n',i);
     break;
